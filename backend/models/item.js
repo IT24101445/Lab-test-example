ff4d2cf0f2324013a51c6af01eae02ea274a681a
Ashan Akalanka <ashanakalanka600@gmail.com>
2026-04-27 11:06:08 +0530

    add to github

--- backend/models/item.js
+++ backend/models/item.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const ItemSchema = new mongoose.Schema({
  name: { type: String, required: true },
  description: { type: String, required: true },
- price: {type: Number, required:true},
-}, { timestamps: true });
+ price: {type: Number, required:true},}, { timestamps: true });
 
 module.exports = mongoose.model('Item', ItemSchema);
